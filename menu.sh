717ae537e64dcfaf67efd45a820098715cda2958
Bonnev <piggov@gmail.com>
2025-03-08 14:24:57 +0200

    Refactored a bit

--- menu.sh
+++ menu.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Function to display the menu
 show_menu() {
     clear
     echo "Use arrow keys to navigate, Enter to select:"
     for i in "${!options[@]}"; do
         if [ $i -eq $selected ]; then
-            printf "\e[7m> ${options[i]}\e[0m\n"  # Inverted colors for selected
+            # printf "\e[7m> ${options[i]}\e[0m\n"  # Inverted colors for selected
+            printf "> ${options[i]}\n"
         else
             echo "  ${options[i]}"
         fi
     done
 }
 
-# Initial setup
+# Options setup
 options=("Option 1" "Option 2" "Option 3" "Quit")
 selected=0
 
@@ -28,35 +28,28 @@ printf "\e[?25l"
 # Main loop
 while true; do
     show_menu
-
-    # Read input
-    read -rsn1 key
+    # Read three characters (to capture escape sequences)
+    read -rsn3 key
     case "$key" in
-        $'\x1b')  # Escape sequence
-            read -rsn2 -t 0.1 key2
-            case "$key2" in
-                '[A') ((selected--))  # Up arrow
-                    [ $selected -lt 0 ] && selected=$((${#options[@]}-1)) ;;
-                '[B') ((selected++))  # Down arrow
-                    [ $selected -ge ${#options[@]} ] && selected=0 ;;
-            esac
+        $'\e[A')  # Up arrow
+            (( selected-- ))
+            if [ "$selected" -lt 0 ]; then
+                selected=$((${#options[@]} - 1))
+            fi
             ;;
-        '')  # Enter key
-            break ;;
+        $'\e[B')  # Down arrow
+            (( selected++ ))
+            if [ "$selected" -ge "${#options[@]}" ]; then
+                selected=0
+            fi
+            ;;
+        "")  # Enter key (when no key is read, read returns an empty string)
+            break
+            ;;
     esac
 done
 
 # Show cursor again
 printf "\e[?25h"
 
-# Handle selection
-case "${options[selected]}" in
-    "Option 1")
-        echo "You chose Option 1" ;;
-    "Option 2")
-        echo "You chose Option 2" ;;
-    "Option 3")
-        echo "You chose Option 3" ;;
-    "Quit")
-        echo "Exiting..." ;;
-esac
+echo "${options[selected]}"
